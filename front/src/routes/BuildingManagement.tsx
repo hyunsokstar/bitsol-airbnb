3451fc8ed2263278a6115e2285a6faa50283a999
hyunsok <hyunsokstar@gmail.com>
2023-02-06 17:40:34 +0900

    update for 0206 1740

--- front/src/routes/BuildingManagement.tsx
+++ front/src/routes/BuildingManagement.tsx
@@ -145,7 +145,6 @@ function BuildingManagement({ }: Props): ReactElement {
                                     asdfasdfa
                                 </GridItem>
                             </Grid>
-
                         </TabPanel>
                     </TabPanels>
                 </Tabs>
