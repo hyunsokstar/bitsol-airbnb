89ccdd795314c35092a1be1c9c462d5d8985b440
hyun <hyunsokstar@gmail.com>
2023-02-13 08:05:04 +0900

    update for readme

--- wishlists/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-
-from .views import Wishlists, WishlistDetail
-
-urlpatterns = [
-    path("", Wishlists.as_view()),
-    path("<int:pk>", WishlistDetail.as_view()),    
-]
